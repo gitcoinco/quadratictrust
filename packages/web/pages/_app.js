69ae6823a1bc1891e2c75d43fe3cd1bbb79be21d
Ivan Molto <ivanmolto@gmail.com>
2021-07-30 16:09:54 +0200

    chore: Add SEO

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -1,9 +1,16 @@
 import "tailwindcss/tailwind.css";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
+import Head from "next/head";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </Layout>
   );
--- packages/web/pages/_document.js
+++ packages/web/pages/_document.js
@@ -1,13 +1,64 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta content="#ffffff" name="theme-color" />
+          <meta content="#ffffff" name="msapplication-TileColor" />
+          <meta
+            content="/favicons/browserconfig.xml"
+            name="msapplication-config"
+          />
+          <meta content="" name="yandex-verification" />
+          <meta content="" name="google-site-verification" />
+          <link href="/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="favicons/site.webmanifest" rel="manifest" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com/"
+            crossOrigin=""
+          />
+          <link rel="preconnect" href="https://pbs.twimg.com" crossOrigin="" />
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@500;600&family=Karla:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:wght@500;600&family=Karla:wght@400;700&display=swap"
             rel="stylesheet"
+            media="print"
+            onLoad="this.media='all'"
+          />
+          <link
+            href="/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <link
+            color="#5fffff"
+            href="/favicons/safari-pinned-tab.svg"
+            rel="mask-icon"
           />
         </Head>
         <body>
