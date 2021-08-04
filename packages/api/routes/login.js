e6541cb10cbaa7a338aba3d8d8f7cdd6f90a5806
yuetloo <contact@yuetloo.com>
2021-08-04 00:24:31 -0300

    redirect to frontend after login

--- packages/api/routes/login.js
+++ packages/api/routes/login.js
@@ -1,44 +1,41 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const Twitter = require('../utils/twitter')
 const crypto = require('../utils/crypto')
 const User = require('../queries/user')
 
 /* login */
 router.get('/', async (req, res, next) => {
-
   try {
     const twitter = new Twitter()
-    const {oauth_token} = await twitter.getRequestToken()
-    res.redirect(`https://api.twitter.com/oauth/authenticate?oauth_token=${oauth_token}`)
-  }
-  catch(err) {
-    console.log('errr', err)  
+    const { oauth_token } = await twitter.getRequestToken()
+    res.redirect(
+      `https://api.twitter.com/oauth/authenticate?oauth_token=${oauth_token}`
+    )
+  } catch (err) {
+    console.log('errr', err)
     next(err)
   }
-});
+})
 
-router.get('/callback', async (req, res, next) => {
-  const { 
-    oauth_verifier: oauthVerifier,
-    oauth_token: oauthToken 
-  } = req.query;
+router.get('/twitter-callback', async (req, res, next) => {
+  const { oauth_verifier: oauthVerifier, oauth_token: oauthToken } = req.query
 
   try {
-    if( !oauthVerifier || !oauthToken ) throw new Error('Missing oauth token')
+    if (!oauthVerifier || !oauthToken) throw new Error('Missing oauth token')
 
     const twitter = new Twitter()
     const token = await twitter.getAccessToken({
       oauthVerifier,
-      oauthToken
+      oauthToken,
     })
 
-    const username = token.screen_name;
+    const username = token.screen_name
 
     const auth = crypto.encrypt({
       username,
       tokenKey: token.oauth_token,
-      tokenSecret: token.oauth_token_secret
+      tokenSecret: token.oauth_token_secret,
     })
 
     const isOptout = await User.isOptout(username)
@@ -46,13 +43,12 @@ router.get('/callback', async (req, res, next) => {
       throw new Error('Not authorized')
     }
 
-    req.session.auth = auth;
-    res.json({ username })
-
+    req.session.auth = auth
+    res.redirect('/callback')
   } catch (err) {
-    console.log('callback error', err)  
-    next({status: 401, message: err.message})
+    console.log('callback error', err)
+    next({ status: 401, message: err.message })
   }
 })
 
-module.exports = router;
+module.exports = router
