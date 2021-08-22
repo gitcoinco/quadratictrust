f6397e1277d8053cfe9beae6cec9eb092e9b1823
Ivan Molto <ivanmolto@gmail.com>
2021-08-22 14:08:33 +0200

    feat: Enhance top ranking badges

--- packages/web/components/second-badge.js
+++ packages/web/components/second-badge.js
@@ -10,11 +10,11 @@ export default function SecondBadge() {
     >
       <path
         d="M0 90V2C0 0.895431 0.895431 0 2 0H38C39.1046 0 40 0.89543 40 2V90L20 78.75L0 90Z"
-        fill="#1E914C"
+        fill="#FEC833"
       />
       <path
         d="M17.5053 44.84H24.9253V47H15.1453V44.78C15.1453 43.22 15.6986 42.0867 16.8053 41.38C17.1786 41.14 17.612 40.9267 18.1053 40.74L20.7653 39.72C21.352 39.4933 21.7786 39.2133 22.0453 38.88C22.3253 38.5333 22.4653 38.1067 22.4653 37.6C22.4653 37.0933 22.2853 36.6733 21.9253 36.34C21.5653 35.9933 21.052 35.82 20.3853 35.82C19.7186 35.82 19.1653 35.98 18.7253 36.3C18.2986 36.62 18.052 37.0933 17.9853 37.72H15.3653C15.392 36.4533 15.8786 35.46 16.8253 34.74C17.692 34.0867 18.8586 33.76 20.3253 33.76C21.672 33.76 22.7853 34.12 23.6653 34.84C24.5586 35.5867 25.0053 36.5 25.0053 37.58C25.0053 38.66 24.732 39.5267 24.1853 40.18C23.652 40.82 22.8186 41.36 21.6853 41.8L18.9853 42.84C18.4653 43.04 18.0853 43.2667 17.8453 43.52C17.6186 43.76 17.5053 44.0733 17.5053 44.46V44.84Z"
-        fill="#FEC833"
+        fill="#0F00B7"
       />
       <line x1="5" y1="59.5" x2="35" y2="59.5" stroke="url(#paint0_linear)" />
       <line x1="5" y1="62.5" x2="35" y2="62.5" stroke="url(#paint1_linear)" />
@@ -29,9 +29,9 @@ export default function SecondBadge() {
           y2="60"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#FEC833" stopOpacity="0" />
-          <stop offset="0.51875" stopColor="#FEC833" />
-          <stop offset="1" stopColor="#FEC833" stopOpacity="0" />
+          <stop stopColor="#0F00B7" stopOpacity="0" />
+          <stop offset="0.51875" stopColor="#0F00B7" />
+          <stop offset="1" stopColor="#0F00B7" stopOpacity="0" />
         </linearGradient>
         <linearGradient
           id="paint1_linear"
@@ -41,9 +41,9 @@ export default function SecondBadge() {
           y2="63"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#FEC833" stopOpacity="0" />
-          <stop offset="0.51875" stopColor="#FEC833" />
-          <stop offset="1" stopColor="#FEC833" stopOpacity="0" />
+          <stop stopColor="#0F00B7" stopOpacity="0" />
+          <stop offset="0.51875" stopColor="#0F00B7" />
+          <stop offset="1" stopColor="#0F00B7" stopOpacity="0" />
         </linearGradient>
         <linearGradient
           id="paint2_linear"
@@ -53,9 +53,9 @@ export default function SecondBadge() {
           y2="20"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#FEC833" stopOpacity="0" />
-          <stop offset="0.51875" stopColor="#FEC833" />
-          <stop offset="1" stopColor="#FEC833" stopOpacity="0" />
+          <stop stopColor="#0F00B7" stopOpacity="0" />
+          <stop offset="0.51875" stopColor="#0F00B7" />
+          <stop offset="1" stopColor="#0F00B7" stopOpacity="0" />
         </linearGradient>
         <linearGradient
           id="paint3_linear"
@@ -65,9 +65,9 @@ export default function SecondBadge() {
           y2="17"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#FEC833" stopOpacity="0" />
-          <stop offset="0.51875" stopColor="#FEC833" />
-          <stop offset="1" stopColor="#FEC833" stopOpacity="0" />
+          <stop stopColor="#0F00B7" stopOpacity="0" />
+          <stop offset="0.51875" stopColor="#0F00B7" />
+          <stop offset="1" stopColor="#0F00B7" stopOpacity="0" />
         </linearGradient>
       </defs>
     </svg>
