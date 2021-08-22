fcb0ca118e340522d21666632bcf68f3b838f986
Ivan Molto <ivanmolto@gmail.com>
2021-08-22 14:09:15 +0200

    feat: Add new website background color

--- packages/web/components/layout.js
+++ packages/web/components/layout.js
@@ -2,10 +2,10 @@ import GameFooter from "./game-footer";
 import GameMenu from "./game-menu";
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="bg-trust-background">
       <GameMenu />
       <main>{children}</main>
       <GameFooter />
-    </>
+    </div>
   );
 }
