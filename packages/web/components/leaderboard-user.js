5159d9088558bde3b9a273457d66820297646e9d
Ivan Molto <ivanmolto@gmail.com>
2021-08-22 14:18:00 +0200

    feat: Make ranking more responsive

--- packages/web/components/leaderboard-user.js
+++ packages/web/components/leaderboard-user.js
@@ -12,7 +12,7 @@ export default function LeaderboardUser(props) {
     <div className="max-w-6xl mx-auto sm:px-2 md:px-2">
       {users.map((user) => (
         <div key={user.rank}>
-          <div className="mb-2 hidden md:px-1 lg:px-4 sm:block">
+          <div className="mb-2 hidden sm:block md:px-1 lg:px-4">
             <div
               className={
                 user.rank < 4
@@ -20,21 +20,21 @@ export default function LeaderboardUser(props) {
                   : "rounded-lg xl:max-w-screen-xl bg-white border-2 border-trust-blue"
               }
             >
-              <div className="h-20 md:h-24 flex flex-row justify-between">
+              <div className="h-16 sm:h-20 md:h-24 flex flex-row justify-between">
                 <div className="flex flex-row items-center">
                   <div className="-mt-2 sm:-mt-4 md:-mt-4 mr-1.5 text-white">
                     {user.rank == 1 && <FirstBadge />}
                     {user.rank == 2 && <SecondBadge />}
                     {user.rank == 3 && <ThirdBadge />}
                     {user.rank > 3 && (
-                      <div className="relative items-center -mt-1">
+                      <div className="relative items-center -mt-4 sm:-mt-1">
                         <Badge />
                         <div className="absolute items-center text-white font-karla font-bold sm:text-md md:text-xl sm:ml-5 sm:-mt-12 md:-mt-14 md:ml-7">
                           <div
                             className={
                               user.rank < 10
                                 ? ""
-                                : "transform rotate-90 relative -ml-2"
+                                : "transform rotate-90 relative -ml-1"
                             }
                           >
                             {user.rank}
@@ -51,7 +51,7 @@ export default function LeaderboardUser(props) {
                     }
                   >
                     <img
-                      className="h-12 w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 rounded-full"
+                      className=" h-16 w-16 sm:h-16 sm:w-16 md:h-14 md:w-14 lg:h-16 lg:w-16 rounded-full"
                       src={user.profileUrl}
                       alt={user.name}
                     />
@@ -59,8 +59,8 @@ export default function LeaderboardUser(props) {
                   <div
                     className={
                       user.rank < 4
-                        ? "flex flex-col w-60 sm:w-44 md:w-64 lg:w-96 text-white"
-                        : "flex flex-col w-60 sm:w-44 md:w-64 lg:w-96 text-trust-blue"
+                        ? "flex flex-col w-60 sm:w-44 md:w-60 lg:w-96 text-white"
+                        : "flex flex-col w-60 sm:w-44 md:w-60 lg:w-96 text-trust-blue"
                     }
                   >
                     <div className="font-karla font-bold sm:text-base md:text-lg lg:text-2xl flex-none truncate">
@@ -101,22 +101,24 @@ export default function LeaderboardUser(props) {
                   <div
                     className={
                       user.rank < 4
-                        ? "flex flex-col sm:w-16 w-32 items-start mr-8 md:mr-0 lg:mr-2 text-white"
-                        : "flex flex-col sm:w-16 w-32 items-start mr-8 md:mr-0 lg:mr-2 text-trust-blue"
+                        ? "flex flex-col sm:w-32 w-32 md:w-32 items-start mr-8 md:mr-0 lg:mr-2 text-white"
+                        : "flex flex-col sm:w-32 w-32 md:w-32 items-start mr-8 md:mr-0 lg:mr-2 text-trust-blue"
                     }
                   >
                     <div className="font-raleway font-semibold sm:text-xl md:text-2xl lg:text-4xl flex-none mr-2 md:mr-4 lining-nums">
                       {user.score}
                     </div>
-                    <div className="font-karla flex-none text-xs">VOTES</div>
+                    <div className="font-karla flex-none text-xs">
+                      VOTES RECEIVED
+                    </div>
                   </div>
                   <Link href={`/handles/${user.username}`}>
-                    <a className="flex sm:-ml-8 md:ml-12 lg:ml-24 mr-2">
+                    <a className="flex sm:-ml-8 md:ml-0 lg:ml-16 mr-2">
                       <button
                         className={
                           user.rank < 4
-                            ? "bg-transparent hover:bg-trust-yellow font-semibold hover:text-trust-blue py-1 px-2 md:py-2 md:px-4 border hover:border-trust-blue rounded mr-4 items-end text-white border-white"
-                            : "bg-transparent hover:bg-trust-yellow font-semibold hover:text-trust-blue py-1 px-2 md:py-2 md:px-4 border hover:border-trust-blue rounded mr-4 items-end text-trust-blue border-trust-blue"
+                            ? "bg-transparent hover:bg-trust-yellow font-semibold hover:text-trust-blue py-1 px-2 md:py-2 md:px-4 border hover:border-trust-blue rounded mr-4 items-end text-white border-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-trust-blue"
+                            : "bg-transparent hover:bg-trust-yellow font-semibold hover:text-trust-blue py-1 px-2 md:py-2 md:px-4 border hover:border-trust-blue rounded mr-4 items-end text-trust-blue border-trust-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-trust-blue"
                         }
                       >
                         VOTE
@@ -176,8 +178,8 @@ export default function LeaderboardUser(props) {
                       <button
                         className={
                           user.rank < 4
-                            ? "bg-transparent hover:bg-trust-yellow font-semibold hover:text-trust-blue py-2 px-8 border hover:border-transparent rounded mt-2 mr-4 lg:mr-8 items-center mx-auto text-white border-white"
-                            : "bg-transparent hover:bg-trust-yellow font-semibold hover:text-trust-blue py-2 px-8 border hover:border-transparent rounded mt-2 mr-4 lg:mr-8 items-center mx-auto text-trust-blue border-trust-blue"
+                            ? "bg-transparent hover:bg-trust-yellow hover:border-trust-blue font-semibold hover:text-trust-blue py-2 px-8 border hover:border-transparent rounded mt-2 mr-4 lg:mr-8 items-center mx-auto text-white border-white"
+                            : "bg-transparent hover:bg-trust-yellow hover:border-trust-blue font-semibold hover:text-trust-blue py-2 px-8 border hover:border-transparent rounded mt-2 mr-4 lg:mr-8 items-center mx-auto text-trust-blue border-trust-blue"
                         }
                       >
                         VOTE
@@ -208,12 +210,12 @@ export default function LeaderboardUser(props) {
                 <div
                   className={
                     user.rank < 4
-                      ? "flex-grow text-white"
-                      : "flex-grow text-trust-blue"
+                      ? "flex-1 text-white"
+                      : "flex-1 text-trust-blue"
                   }
                 >
                   <div className="flex flex-col items-center">
-                    <div className="font-raleway font-semibold text-3xl flex-none">
+                    <div className="font-raleway font-semibold text-3xl flex-none lining-nums">
                       {user.credits}
                     </div>
                     <div className="mb-6 font-karla flex-none text-xs">
@@ -221,16 +223,19 @@ export default function LeaderboardUser(props) {
                     </div>
                   </div>
                 </div>
-                <div className="ml-4 mr-4"></div>
+                <div className="flex flex-col items-center">
+                  {user.rank < 4 && <WhiteLine />}
+                  {user.rank > 3 && <BlueLine />}
+                </div>
                 <div
                   className={
                     user.rank < 4
-                      ? "flex-grow text-white"
-                      : "flex-grow text-trust-blue"
+                      ? "flex-1 text-white"
+                      : "flex-1 text-trust-blue"
                   }
                 >
                   <div className="flex flex-col items-center">
-                    <div className="font-raleway font-semibold text-3xl flex-none">
+                    <div className="font-raleway font-semibold text-3xl flex-none lining-nums">
                       {user.score}
                     </div>
                     <div className="mb-6 font-karla flex-none text-xs">
