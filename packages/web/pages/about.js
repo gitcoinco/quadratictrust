d19cac61654f416a2e9c59fb84caac3c85d6c879
Ivan Molto <ivanmolto@gmail.com>
2021-08-22 14:21:49 +0200

    feat: Enhance about page

--- packages/web/pages/about.js
+++ packages/web/pages/about.js
@@ -1,6 +1,11 @@
 import { useContext } from "react";
 import { UserContext } from "../lib/UserContext";
 import Head from "next/head";
+import Image from "next/image";
+import quadratic from "../public/quadratic.svg";
+import people from "../public/people.svg";
+import participation from "../public/participation.svg";
+import trust from "../public/trust.svg";
 
 export default function About() {
   const [user] = useContext(UserContext);
@@ -13,293 +18,10 @@ export default function About() {
           content="Use the trust you have built to support people you trust"
         />
         <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="canonical" href="https://quadratictrust.com/about" />
       </Head>
       <div className="flex flex-col max-w-4xl mx-auto justify-center px-4">
-        <svg
-          className="h-full w-full"
-          width="867"
-          height="416"
-          viewBox="0 0 867 416"
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-        >
-          <path
-            id="Path"
-            d="M490 147 C494.5 145.5 497.5 151.5 495 156 493 159.6 490.833 160.5 490 160.5"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-1"
-            d="M378.906 147 C374.406 145.5 371.406 151.5 373.906 156 375.906 159.6 378.073 160.5 378.906 160.5"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-2"
-            d="M407 178 C423.079 192.366 445.708 192.964 460 178"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-3"
-            d="M42.698 415.601 L120.698 356.601"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-4"
-            d="M50.698 0.399 L128.698 59.399"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-5"
-            d="M840.302 415.601 L762.302 356.601"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-6"
-            d="M834.302 0.399 L756.302 59.399"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-7"
-            d="M432.5 51 L432.5 9"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-8"
-            d="M432.5 380 L432.5 347"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-9"
-            d="M92 186.5 L0 186.5"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-10"
-            d="M867 176.5 L794 176.5"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-11"
-            d="M490.5 156.5 C490.5 187.428 465.428 212.5 434.5 212.5 403.572 212.5 378.5 187.428 378.5 156.5 378.5 125.572 403.572 100.5 434.5 100.5 465.428 100.5 490.5 125.572 490.5 156.5 Z"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-12"
-            d="M454.5 251.5 C454.5 239.902 445.098 230.5 433.5 230.5 421.902 230.5 412.5 239.902 412.5 251.5 412.5 263.098 421.902 272.5 433.5 272.5 445.098 272.5 454.5 263.098 454.5 251.5 Z"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-13"
-            d="M448.5 251.5 C448.5 243.216 441.784 236.5 433.5 236.5 425.216 236.5 418.5 243.216 418.5 251.5 418.5 259.784 425.216 266.5 433.5 266.5 441.784 266.5 448.5 259.784 448.5 251.5 Z"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-14"
-            d="M435.698 242.346 L434.482 251.083 433.211 251.083 435.387 242.283 Z"
-            fill="#0f00b7"
-            fillOpacity="1"
-            stroke="none"
-          />
-          <path
-            id="Path-15"
-            d="M434.481 251.083 L432.177 259.946 431.866 259.884 433.21 251.083 Z"
-            fill="#0f00b7"
-            fillOpacity="1"
-            stroke="none"
-          />
-          <path
-            id="Path-16"
-            d="M433.369 251.756 L428.112 247.716 428.287 247.451 433.763 250.763 Z"
-            fill="#0f00b7"
-            fillOpacity="1"
-            stroke="none"
-          />
-          <path
-            id="Path-17"
-            d="M434.322 251.12 L442.097 255.98 441.938 256.255 433.687 252.073 Z"
-            fill="#0f00b7"
-            fillOpacity="1"
-            stroke="none"
-          />
-          <path
-            id="Path-18"
-            d="M433.723 251.842 L425.567 254.075 425.472 253.772 433.369 250.802 Z"
-            fill="#0f00b7"
-            fillOpacity="1"
-            stroke="none"
-          />
-          <path
-            id="Path-19"
-            d="M433.718 250.643 L440.509 249.306 440.595 249.612 433.846 251.915 Z"
-            fill="#0f00b7"
-            fillOpacity="1"
-            stroke="none"
-          />
-          <path
-            id="Path-20"
-            d="M585.5 105.5 C585.5 120.376 577.3 135.632 565.963 147.187 554.619 158.749 540.24 166.5 528 166.5 515.825 166.5 506.715 158.821 500.622 147.304 494.527 135.782 491.5 120.493 491.5 105.5 491.5 90.583 497.485 76.699 506.553 66.546 515.625 56.388 527.747 50 540 50 564.488 50 585.5 75.579 585.5 105.5 Z"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-21"
-            d="M455 145 C455 146.657 453.657 148 452 148 450.343 148 449 146.657 449 145 449 143.343 450.343 142 452 142 453.657 142 455 143.343 455 145 Z"
-            fill="#0f00b7"
-            fillOpacity="1"
-            stroke="none"
-          />
-          <path
-            id="Path-22"
-            d="M420 145 C420 146.657 418.657 148 417 148 415.343 148 414 146.657 414 145 414 143.343 415.343 142 417 142 418.657 142 420 143.343 420 145 Z"
-            fill="#0f00b7"
-            fillOpacity="1"
-            stroke="none"
-          />
-          <path
-            id="Path-23"
-            d="M462 108 C417 129.5 382.25 131.59 371 132"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-24"
-            d="M435 170 L441 166.348 435 156"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-25"
-            d="M462 108 C465 128.5 478 139 488.5 142"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-26"
-            d="M467.5 204 C467.333 207.5 460.7 220.7 447.5 235.5 448.5 237 452 239 453.5 245 461 235.5 475.5 221.5 477.5 207.5 Z"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-27"
-            d="M400 203 C400.167 206.5 408.3 219.2 421.5 234 417.5 237.5 415.5 239 414 244 402.5 232 392.5 221.5 390 205.5 Z"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-28"
-            d="M490.5 160.5 C490.5 160.5 494.929 169.5 496.5 180.5 498.071 191.5 498.333 200.667 498 203.5"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-          <path
-            id="Path-29"
-            d="M384 131.5 C384 131.5 380.968 137.625 378 146.5 M363 199.5 C367.5 192.5 369.627 184.5 371.127 174 372.627 163.5 373.234 163.874 374.981 158"
-            fill="none"
-            stroke="#0f00b7"
-            strokeWidth="1"
-            strokeOpacity="1"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-          />
-        </svg>
+        <Image src={quadratic} alt="Quadratic" />
         <div>
           <div className="flex mt-4 sm:hidden justify-center">
             Quadratic Trust is a challenge where members of the
@@ -344,16 +66,7 @@ export default function About() {
       </div>
       <div className="flex flex-row max-w-4xl mx-auto justify-center space-x-4 px-4">
         <div className="flex flex-col mt-4">
-          <svg
-            className="h-full w-full"
-            width="300"
-            height="240"
-            viewBox="0 0 300 240"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="300" height="240" fill="#C4C4C4" fillOpacity="0.4" />
-          </svg>
+          <Image className="flex-1" src={participation} alt="Participation" />
           <div className="hidden sm:flex flex-col">
             <div className="justify-center mx-auto">To participate,</div>
             <div className="justify-center mx-auto">share a tweet</div>
@@ -373,17 +86,34 @@ export default function About() {
             <div className="justify-center mx-auto mb-4">your vote.</div>
           </div>
         </div>
-        <div className="flex">
+        <div className="hidden sm:flex">
           <svg
             width="1"
-            height="410"
-            viewBox="0 0 1 410"
+            height="660"
+            viewBox="0 0 1 660"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
             <line
               x1="0.5"
-              y1="410"
+              y1="660"
+              x2="0.500018"
+              y2="-2.18557e-08"
+              stroke="#0F00B7"
+            />
+          </svg>
+        </div>
+        <div className="flex sm:hidden">
+          <svg
+            width="1"
+            height="437"
+            viewBox="0 0 1 437"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="0.5"
+              y1="437"
               x2="0.500018"
               y2="-2.18557e-08"
               stroke="#0F00B7"
@@ -392,16 +122,7 @@ export default function About() {
         </div>
 
         <div className="flex flex-col mt-4">
-          <svg
-            className="h-full w-full"
-            width="300"
-            height="240"
-            viewBox="0 0 300 240"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="300" height="240" fill="#C4C4C4" fillOpacity="0.4" />
-          </svg>
+          <Image src={trust} alt="trust" />
           <div className="hidden sm:flex flex-col">
             <div className="justify-center mx-auto">
               Your follower count on Twitter
@@ -423,9 +144,9 @@ export default function About() {
               number of voting credits
             </div>
             <div className="justify-center mx-auto">you have to distribute</div>
-            <div className="justify-center mx-auto">amongst the people</div>
+            <div className="justify-center mx-auto">amongst the people the</div>
             <div className="justify-center mx-auto mb-4">
-              the people you want to vote for.
+              people you want to vote for.
             </div>
           </div>
         </div>
@@ -458,16 +179,7 @@ export default function About() {
         >
           <line y1="0.5" x2="700" y2="0.5" stroke="#0F00B7" />
         </svg>
-        <svg
-          className="h-full w-full mt-8"
-          width="700"
-          height="280"
-          viewBox="0 0 700 280"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect width="700" height="280" fill="#C4C4C4" fillOpacity="0.4" />
-        </svg>
+        <Image src={people} alt="People" />
         <div>
           <div className="flex sm:hidden justify-center">
             Just like with Quadratic Funding, the number of people
@@ -539,7 +251,7 @@ export default function About() {
           <div className="flex justify-center">
             <a
               href="https://quadratictrust.com/api/login"
-              className="font-raleway tracking-widest text-xl bg-trust-blue hover:bg-trust-yellow border-2 font-semibold hover:text-trust-blue text-white border-trust-blue px-12 py-4 hover:border-trust-blue rounded-md"
+              className="font-raleway tracking-widest text-xl bg-trust-blue hover:bg-trust-yellow border-2 font-semibold hover:text-trust-blue text-white border-trust-blue px-12 py-4 hover:border-trust-blue rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-trust-blue"
             >
               JOIN
             </a>
